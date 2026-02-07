56135f66416f1bb50156daa6cedd2231e9843b11
venkat <devopsaws84s@gmail.com>
2026-02-07 18:56:00 +0530

    Terraform

--- Main.tf
+++ Main.tf
@@ -35,8 +35,8 @@ resource "aws_security_group" "main" {
 #   ip_protocol       = "-1" # semantically equivalent to all ports
 # }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = "::/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
